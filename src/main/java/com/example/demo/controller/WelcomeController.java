46418a9fd2c73725dc31b5796a45d1647846e310
deeptej <deeptejgawand@gmail.com>
2022-08-03 23:35:20 +0530

    update

--- src/main/java/com/example/demo/controller/WelcomeController.java
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/user")
+@RestController
+@RequestMapping("/user")
 public class WelcomeController {
 	@GetMapping("/msg")
 	public String getMessage() {
