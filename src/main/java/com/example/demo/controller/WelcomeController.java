48efb66b2516b635bdd576c94968d580c26f40f7
deeptej <deeptejgawand@gmail.com>
2022-08-03 22:52:29 +0530

    first commit

--- src/main/java/com/example/demo/controller/WelcomeController.java
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -1,5 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/user")
 public class WelcomeController {
+	@GetMapping("/msg")
+	public String getMessage() {
+		return "welcome";
+	}
 
 }
